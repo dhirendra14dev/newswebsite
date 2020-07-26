f250f4b71e7a0e40d0313ba4ea6bd7b11c37c18e
dhirendra singh <dhirendra@dhirendras-iMac.local>
2020-07-26 18:21:46 +0530

    latest

--- latest/admin.py
+++ latest/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Latest
+# from .models import Latest
 
-class LatestAdmin(admin.ModelAdmin):
-  list_display = ('id',)
+# class LatestAdmin(admin.ModelAdmin):
+#   list_display = ('id',)
 
 
-admin.site.register(Latest,LatestAdmin) 
+# admin.site.register(Latest,LatestAdmin) 
