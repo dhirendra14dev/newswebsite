e7dc7f5b530b9c275bbb7e10d26ed51211127288
Dhirendra Singh <dhirendrasingh@Dhirendras-iMac.local>
2020-09-12 13:29:19 +0530

    crossword-mobile-keypad

--- crossword/urls.py
+++ crossword/urls.py
@@ -1,8 +1,8 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#   path('', views.crossword, name='crossword'),
-#   path('crossword_check', views.crossword_check, name='crossword_check'),
+urlpatterns = [
+  path('', views.crossword, name='crossword'),
+  path('crossword_check', views.crossword_check, name='crossword_check'),
   
-# ]
+]
